a782abe08e07c5905280c6b7bfde0626d96f154d
Jacob Bills <jbills@bustedware.com>
2024-10-06 15:42:19 -0400

    removed unused resources from main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -15,33 +15,6 @@ provider "aws" {
     secret_key = var.secret_key
 }
 
-// AWS
-
-// security group
 resource "aws_internet_gateway" "gw" {
 #   vpc_id = aws_vpc.aws.id
 }
-
-# resource "aws_vpc" "aws" {
-#     cidr_block = "10.114.0.0/24"
-# }
-
-# resource "aws_subnet" "aws" {
-#     vpc_id     = aws_vpc.aws.id
-#     cidr_block = cidrsubnet("10.114.0.0/24", 1, 0)
-#     map_public_ip_on_launch = true
-# }
-
-# resource "aws_main_route_table_association" "aws" {
-#   vpc_id         = aws_vpc.aws.id
-#   route_table_id = aws_route_table.aws.id
-# }
-
-# resource "aws_route_table" "aws" {
-#     vpc_id = aws_vpc.aws.id
-    
-#     route {
-#         cidr_block = "10.114.0.0/24"
-#         gateway_id = "local"
-#     }
-# }
